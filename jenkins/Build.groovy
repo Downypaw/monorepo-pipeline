92d7132e5d76b35c4c434dac23db47dfda9aeedb
whoisYeshua <alien19987@gmail.com>
2023-07-29 16:13:49 +0300

    another

--- jenkins/Build.groovy
+++ jenkins/Build.groovy
@@ -19,9 +19,10 @@ node {
 
     stage('Build Docker Image') {
         // Optional: Push the Docker image to a Docker registry
+        println('before registry')
         docker.withRegistry('https://registry.hub.docker.com', '0f5d8e3e-e52e-472f-be6e-cc4e558cd32c') {
             // The Dockerfile is in the current directory
-            echo 'inside registry'
+            println('inside registry')
             def image = docker.build("monorepo-${env.module}:${env.BUILD_NUMBER}", '--build-args=${evn.module}')
             echo 'after build'
             image.push("${env.BUILD_NUMBER}")
