907be4fe2b54b7d91f40682fb20a88460cea221d
whoisYeshua <alien19987@gmail.com>
2023-07-29 16:10:20 +0300

    add docker step

--- jenkins/Build.groovy
+++ jenkins/Build.groovy
@@ -16,4 +16,15 @@ node {
         // Print all files and directories in the current workspace
         sh 'ls -l'
     }
+
+    stage('Build Docker Image') {
+        // Optional: Push the Docker image to a Docker registry
+        docker.withRegistry('https://registry.hub.docker.com', '0f5d8e3e-e52e-472f-be6e-cc4e558cd32c') {
+            // The Dockerfile is in the current directory
+            def image = docker.build("monorepo-${env.module}:${env.BUILD_NUMBER}", '--build-args=${evn.module}')
+
+            image.push("${env.BUILD_NUMBER}")
+            image.push("latest")
+        }
+    }
 }
